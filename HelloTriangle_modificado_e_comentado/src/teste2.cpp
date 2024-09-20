66a91cc6ac8a7a7f8d567486c5c8af8d76cc78e8
leandroboniatti <boniatti@hotmail.com>
2024-09-20 10:01:27 -0300

    Subida do HelloTriangle

---
+++ HelloTriangle_modificado_e_comentado/src/teste2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+using namespace std;
+	
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Essencial para computadores da Apple
+  
+    return 0;
+}

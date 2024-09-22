3ad022be9f03029f4add67259b832171d34d45eb
leandroboniatti <boniatti@hotmail.com>
2024-09-21 23:49:01 -0300

    acerto dll

--- HelloTriangle_modificado_e_comentado/src/teste2.cpp
+++ HelloTriangle_modificado_e_comentado/src/teste2.cpp
@@ -10,10 +10,13 @@ using namespace std;
 
 int main()
 {
+
+    cout << "teste";
+
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Essencial para computadores da Apple
   
     return 0;
